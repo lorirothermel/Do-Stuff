a38cd55580f1e81fd62abceb198b5b15bf13a40e
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-05-03 16:19:26 -0400

    Initial Commit

---
+++ Do Stuff/Do_StuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  Do_StuffApp.swift
+//  Do Stuff
+//
+//  Created by Lori Rothermel on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Do_StuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
